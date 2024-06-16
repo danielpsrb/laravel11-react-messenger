62ad0eb6f1761d36f0d2e678843eff639f3e42c1
danielpsrb <danielpsrb01@gmail.com>
2024-06-16 22:29:18 +0700

    update routes web dan routes channels

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->group(function() {
-    Route::get('/', [HomeController::class, 'home'])->name('dashboard');
+    Route::get('/', [HomeController::class, 'home'])->name('home');
 
     Route::get('/user/{user}', [MessageController::class, 'byUser'])->name('chat.user');
     Route::get('/group/{group}', [MessageController::class, 'byGroup'])->name('chat.group');
@@ -16,11 +16,6 @@ Route::middleware(['auth', 'verified'])->group(function() {
     Route::delete('/message/{message}', [MessageController::class, 'destroy'])->name('message.destroy');
     Route::get('/message/older/{message}', [MessageController::class, 'loadOlder'])->name('message.loadOlder');
 });
-
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
